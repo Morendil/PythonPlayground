8f7b48bd769e6d7e9d7f1f33927cc2a909caa9c9
Laurent Bossavit <laurent.git@bossavit.com>
2019-04-16 17:01:38 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 
 from setuptools import setup, find_namespace_packages
 
-setup(name="Playground",
+setup(
+    name="Playground",
     version="1.0.0",
     package_dir={'': 'src'},
-    packages=find_namespace_packages(where='src')
-    extras_require = {
+    packages=find_namespace_packages(where='src'),
+    extras_require={
         'dev': [],
         },
-    install_requires = []
+    install_requires=[]
     )
